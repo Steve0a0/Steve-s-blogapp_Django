430c7546839601553f1437560e34b8238cbf6111
Steve0a0 <stephenangelo4@gmail.com>
2023-11-05 01:50:39 +0530

    Deploying to Railway again

--- blog/settings.py
+++ blog/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -78,10 +78,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
+        'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'PORT': '5432'
+        'PASSWORD': '2ADdg*B2AaC-Ee*36gc2C1deC6ba**CB',
+        'HOST' : 'roundhouse.proxy.rlwy.net',
+        'PORT': '34093'
     }
 }
 
