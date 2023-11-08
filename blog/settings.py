c51caf099eac3538acf828727bf9e9a05fb9a34b
Steve0a0 <stephenangelo4@gmail.com>
2023-11-08 15:36:51 +0000

    Deploying to Railway again16

--- blog/settings.py
+++ blog/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
+        'NAME': 'blogsite',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'PORT': '5432'
